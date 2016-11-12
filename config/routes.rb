17848c89218f805037c9e35e5825ab684a7a1e68
Sunisa Rimcharoen <palm.sunisa@gmail.com>
2016-11-12 10:01:16 +0700

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  Rails.application.routes.draw do
+   root 'application#hello'
+  end 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
